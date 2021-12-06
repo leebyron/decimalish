423ff0fb1f364cd0a254e2bb6b2b4fa43b4ff8fc
Lee Byron <lee@leebyron.com>
2021-12-05 23:51:01 -0800

    better icons

--- website/build.tsx
+++ website/build.tsx
@@ -130,9 +130,10 @@ const Index = () =>
       </title>
       <meta charset="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
+      <meta name="theme-color" content="#56585b" />
+      <link rel="icon" href="favicon.png" type="image/png" />
+      <link rel="icon" href="favicon.svg" type="image/svg+xml" />
+      <link rel="manifest" href="manifest.json" />
       <style innerHTML={fs.readFileSync(ROOT_DIR + 'style.css', 'utf8')} />
       <script innerHTML={fs.readFileSync(path.resolve(ROOT_DIR, '../dist/decimalish.min.js'), 'utf8')} />
     </head>
