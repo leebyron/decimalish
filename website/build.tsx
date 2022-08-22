06c11d436ab4ad4827764aa9b91976d74ceedb15
Lee Byron <lee@leebyron.com>
2022-08-22 09:31:27 -0700

    prettier

--- website/build.tsx
+++ website/build.tsx
@@ -708,7 +708,9 @@ const Details = ({ children, forId }: any) => (
               document.currentScript.remove()
               const update = () => {
                 const hash = decodeURIComponent(window.location.hash.slice(1))
-                if (hash && hash === ${JSON.stringify(forId)}) details.open = true
+                if (hash && hash === ${JSON.stringify(
+                  forId
+                )}) details.open = true
               }
               window.addEventListener('hashchange', update)
               update()
